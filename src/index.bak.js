809d6c8d96b4daf221487ee06f7283b08dd10b8f
kitety <aizaizuori@foxmail.com>
2018-12-24 23:03:19 +0800

    todolist

---
+++ src/index.bak.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { action, observable } from 'mobx'
+import PropTypes from 'prop-types'
+//被修饰的数组不是真正的数组，现在已经修复，不会报错了
+import { PropTypes as ObservablePropTypes, observer } from 'mobx-react'
+
+class Store {
+  @observable cache = { queue: [] }
+  // action 写方法
+  @action.bound refresh () {
+    this.cache.queue.push(1)
+  }
+}
+const store = new Store()
+
+// 核心修饰，有用到queue属性
+@observer
+class Bar extends Component {
+  //被修饰的数组不是真正的数组，现在已经修复，不会报错了
+  static propTypes = {
+    queue: PropTypes.array
+  }
+  render () {
+    const { queue } = this.props
+    return <span>{queue.length}</span>
+  }
+}
+// 仅仅修饰这个没用，因为Foo没有用到queue属性
+@observer
+class App extends Component {
+  static propTypes = {
+    cache: PropTypes.object
+  }
+  render () {
+    const { cache } = this.props
+    return (
+      <div>
+        <Bar queue={cache.queue} />
+        <button onClick={this.props.refresh}>Refresh</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App cache={store.cache} refresh={store.refresh} />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- src/index.js
+++ src/index.js
@@ -1,51 +1,116 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {  action, observable } from 'mobx'
+import { action, observable, computed } from 'mobx'
 import PropTypes from 'prop-types'
 //被修饰的数组不是真正的数组，现在已经修复，不会报错了
-import { PropTypes as ObservablePropTypes,observer } from 'mobx-react'
+import { PropTypes as ObservablePropTypes, observer } from 'mobx-react'
+import './App.css'
 
+class Todo {
+  id = Math.random()
+  @observable title = ''
+  @observable finished = false
+  @action.bound toggle () {
+    this.finished = !this.finished
+  }
+  constructor(title) {
+    this.title = title
+  }
+}
 class Store {
-  @observable cache = { queue: [] }
-  @action.bound refresh () {
-    this.cache.queue.push(1)
+  @observable todos = []
+  @action.bound createTodo (title) {
+    //  放在数组前面
+    this.todos.unshift(new Todo(title))
+  }
+  @action.bound removeTodo (todo) {
+    //  放在数组前面
+    this.todos.remove(todo)
+  }
+  @computed get left () {
+    return this.todos.filter(todo => !todo.finished).length
   }
 }
-const store = new Store()
-
-// 核心修饰，有用到queue属性
+var store = new Store()
 @observer
-class Bar extends Component {
-  //被修饰的数组不是真正的数组，现在已经修复，不会报错了
+class TodoItem extends Component {
   static propTypes = {
-    queue: PropTypes.array
+    todo: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      finished: PropTypes.bool.isRequired
+    })
+  }
+  handleClick = e => {
+    this.props.todo.toggle()
   }
   render () {
-    const { queue } = this.props
-    return <span>{queue.length}</span>
-  }
-}
-// 仅仅修饰这个没用，因为Foo没有用到queue属性
-@observer
-class App extends Component {
-  static propTypes = {
-    cache: PropTypes.object
-  }
-  render () {
-    const { cache } = this.props
+    const { todo } = this.props
     return (
       <div>
-        <Bar queue={cache.queue} />
-        <button onClick={this.props.refresh}>Refresh</button>
+        <input type="Checkbox" className="toggle" checked={todo.finished} onChange={this.handleClick}  />
+        <span className={['title', todo.finished && 'finished'].join(' ')}>{todo.title}</span>
+      </div>
+    )
+  }
+}
+@observer
+class TodoList extends Component {
+  static propTypes = {
+    store: PropTypes.shape({
+      createTodo: PropTypes.func,
+      todos: ObservablePropTypes.observableArrayOf(ObservablePropTypes.observableObject).isRequired
+    }).isRequired
+  }
+  state = {
+    inputVal: ''
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    let { store } = this.props
+    store.createTodo(this.state.inputVal)
+    this.setState({
+      inputVal: ''
+    })
+  }
+  handleChange = e => {
+    var inputVal = e.target.value
+    this.setState({
+      inputVal
+    })
+  }
+  render () {
+    const { store } = this.props
+    const todos = store.todos
+    return (
+      <div className="todo-list">
+        <header>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" onChange={this.handleChange} value={this.state.inputVal} className="input" />
+          </form>
+        </header>
+        <ul>
+          {todos.map(todo => {
+            return (
+              <li className="todo-item" key={todo.id}>
+                <TodoItem todo={todo} />
+                <span className="remove" onClick={e => store.removeTodo(todo)}>X</span>
+              </li>
+            )
+          })}
+        </ul>
+        <footer>
+          <div>{store.left} item(s) unfinished!</div>
+        </footer>
       </div>
     )
   }
 }
 
-ReactDOM.render(<App cache={store.cache} refresh={store.refresh} />, document.getElementById('root'));
+
+// 为了保证单独的数据来源，就用传参
+ReactDOM.render(<TodoList store={store} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
